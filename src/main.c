c19ac89ae5a3302f2c75d57edcc272202464783d
Maciej Wozniakowski <maciejwozniakowski02@gmail.com>
2026-03-22 18:41:02 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,9 +4,14 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  char command_buffer[100];
 
   // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  while(1) {
+    printf("$ ");
+    fgets(command_buffer, 100, stdin);
+    printf("%s: command not found\n", command_buffer);
+  }
 
   return 0;
 }
