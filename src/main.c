c69816a1a4e9fc4cc32c803483c3dc947b91838b
Maciej Wozniakowski <maciejwozniakowski02@gmail.com>
2026-03-22 19:00:48 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
@@ -10,11 +11,10 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   while(1) {
     printf("$ ");
-    read(STDIN_FILENO, command_buffer, 99);
-    command_buffer[99] = '\0';
-    // fgets(command_buffer, 100, stdin);
-    // command_buffer[strcspn(command_buffer, "\n")] = '\0';
-    printf("%s: command not found", command_buffer);
+    ssize_t bytes_read = read(STDIN_FILENO, command_buffer, 99);
+    command_buffer[bytes_read] = '\0';
+    command_buffer[strcspn(command_buffer, "\n")] = '\0';
+    printf("%s: command not found \n", command_buffer);
   }
 
   return 0;
