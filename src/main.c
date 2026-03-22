28f5488f657eff689515c72431a558ddc4b914ba
Maciej Wozniakowski <maciejwozniakowski02@gmail.com>
2026-03-22 19:25:59 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -14,6 +14,17 @@ int main(int argc, char *argv[]) {
     ssize_t bytes_read = read(STDIN_FILENO, command_buffer, 99);
     command_buffer[bytes_read] = '\0';
     command_buffer[strcspn(command_buffer, "\n")] = '\0';
+    //parse the command
+    // int i = 0;
+    // while(command_buffer[i] != '\0') {
+    //   if (command_buffer[i] != ' ') {
+    //     char *args[10];
+
+    //     args[0] = command_buffer;
+    //     args[1] = &command_buffer[i + 1];
+    //   }
+    //   i++;
+    // }
     printf("%s: command not found \n", command_buffer);
   }
 
