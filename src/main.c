b5d0334a3b7cb52498f095052bd47c461a40d479
Maciej Wozniakowski <maciejwozniakowski02@gmail.com>
2026-03-22 19:52:08 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -12,10 +12,22 @@ int main(int argc, char *argv[]) {
   while(1) {
     printf("$ ");
     ssize_t bytes_read = read(STDIN_FILENO, command_buffer, 99);
-    command_buffer[bytes_read] = '\0';
+    //add two arrays, one for command and the other for arguments
+    char command[10];
+    char arguments[100];
+    //go through bytes_read until you find a space and add everything that you parsed to command
+    int i = 0;
+    while(command_buffer[i] != ' ' && i < bytes_read) {
+      command[i] = command_buffer[i];
+      i++;
+    }
+    command[i] = '\0';
+    printf("Command: %s\n", command);
     command_buffer[strcspn(command_buffer, "\n")] = '\0';
-    if(strncmp(command_buffer, "exit", 4) == 0) {
+    if(strncmp(command, "exit", 4) == 0) {
       break;
+    } else if (strncmp(command, "echo", 4) == 0) {
+      printf("%s, \n", command );
     }else{
       printf("%s: command not found \n", command_buffer);
     }
